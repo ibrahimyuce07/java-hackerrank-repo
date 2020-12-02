4e2ebd5457245f08300b5e45f7f51b8188ce2400
ibrahim yuce <ibrahimyucee@outlook.com>
2020-12-02 23:37:43 +0300

    introduction

---
+++ Introduction/src/com/EndOfFile.java
@@ -0,0 +1,14 @@
+package com;
+
+import java.util.*;
+
+public class EndOfFile {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		for (int i = 1; scan.hasNext() == true; i++) {
+			System.out.println(i + " " + scan.nextLine());
+		}
+		scan.close();
+	}
+}
---
+++ Introduction/src/com/WelcomeToJava.java
@@ -0,0 +1,13 @@
+package com;
+public class WelcomeToJava {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World.");
+		System.out.println("Hello, Java.");
+	}
+}
+
+//You must print two lines of output:
+//
+//Print Hello, World. on the first line.
+//Print Hello, Java. on the second line.
